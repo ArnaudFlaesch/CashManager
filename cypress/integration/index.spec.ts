bb7e949270feebe7c50bc6e2c5baca34c0c4142e
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-02-07 09:52:02 +0000

    CM-5 Add Cypress report

--- cypress/integration/index.spec.ts
+++ cypress/integration/index.spec.ts
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 describe('Tests de la page Commandes', () => {
   it('Should display the title', () => {
     cy.visit('/').get('span').should('have.text', 'cash-manager');
