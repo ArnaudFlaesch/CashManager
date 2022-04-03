0cbadab236a812252ce305a4f84eaeeb06a9db42
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-04-03 18:20:00 +0200

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,11 @@
 function isTestMode(): boolean {
+  console.log(!!(window as any).Cypress);
   return !!(window as any).Cypress;
 }
 
 export const environment = {
   production: true,
   backend_url: isTestMode()
-    ? 'http://docker:8080'
+    ? 'http://backend'
     : 'https://dash-webservices.herokuapp.com'
 };
