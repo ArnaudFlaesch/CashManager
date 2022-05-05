c34ae994041c0242db78620cd8641cfa38d5b0e3
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-05-05 23:14:41 +0200

    deploy

--- src/app/label-list/label-list.component.spec.ts
+++ src/app/label-list/label-list.component.spec.ts
@@ -1,11 +1,15 @@
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import {
+  byText,
   createComponentFactory,
   createHttpFactory,
+  HttpMethod,
   Spectator,
   SpectatorHttp
 } from '@ngneat/spectator/jest';
+import { environment } from '../../environments/environment';
+import { Label } from '../model/Label';
 import { LabelService } from '../services/label.service/label.service';
 import { ErrorHandlerService } from './../services/error.handler.service';
 
@@ -30,5 +34,19 @@ describe('LabelListComponent', () => {
 
   it('should create', () => {
     expect(spectator.component).toBeTruthy();
+  });
+
+  it('Should delete a label', () => {
+    const labelData = [new Label(1, 'Courses')];
+    spectator.component.labels = labelData;
+    expect(spectator.component.labels.length).toEqual(1);
+    spectator.component.openDeleteLabelDialog(labelData[0].id);
+    spectator.component.update();
+    spectator.click(byText('Valider'));
+    const deleteLabelRequest = labelService.expectOne(
+      `${environment.backend_url}/label/deleteLabel`,
+      HttpMethod.DELETE
+    );
+    deleteLabelRequest.flush(null);
   });
 });
