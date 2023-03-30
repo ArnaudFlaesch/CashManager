519e379706955560e06ab3b0375121299f3dcef3
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2023-03-30 16:58:55 +0200

    lint

--- cypress/e2e/login.spec.ts
+++ cypress/e2e/login.spec.ts
@@ -6,49 +6,33 @@ describe('Login tests', () => {
   beforeEach(() => cy.intercept('POST', '/auth/login').as('login').visit('/'));
 
   it('Should fail to login', () => {
-    cy.get('#loginButton')
-      .should('be.disabled')
-      .get('#inputUsername')
-      .type('test')
-      .get('#inputPassword')
-      .type('test')
-      .get('#loginButton')
-      .should('be.enabled')
-      .click()
-      .wait('@login')
-      .then((request: Interception) => {
-        expect(request.response.statusCode).to.equal(401);
-        cy.shouldDisplayErrorMessage(
-          "Erreur lors de la connexion de l'utilisateur."
-        );
-      });
+    cy.get('#loginButton').should('be.disabled');
+    cy.get('#inputUsername').type('test');
+    cy.get('#inputPassword').type('test');
+    cy.get('#loginButton').should('be.enabled').click();
+    cy.wait('@login').then((request: Interception) => {
+      expect(request.response.statusCode).to.equal(401);
+      cy.shouldDisplayErrorMessage(
+        "Erreur lors de la connexion de l'utilisateur."
+      );
+    });
   });
 
   it('Should login and logout', () => {
-    cy.get('#inputUsername')
-      .clear()
-      .type('admintest')
-      .get('#inputPassword')
-      .clear()
-      .type('adminpassword')
-      .get('#loginButton')
-      .should('be.enabled')
-      .click()
-      .wait('@login')
-      .then((request: Interception) => {
-        expect(request.response.statusCode).to.equal(200);
-        cy.url()
-          .should('be.equal', `${Cypress.config('baseUrl')}home`)
-          .get('#logoutButton')
-          .click()
-          .waitUntil(() =>
-            cy
-              .get('#loginPageTitle')
-              .should('have.text', 'CashManager')
-              .url()
-              .should('be.equal', `${Cypress.config('baseUrl')}login`)
-          )
-          .then(() => expect(localStorage.getItem('user')).to.be.null);
-      });
+    cy.get('#inputUsername').type('admintest');
+    cy.get('#inputPassword').type('adminpassword');
+    cy.get('#loginButton').should('be.enabled').click();
+    cy.wait('@login').then((request: Interception) => {
+      expect(request.response.statusCode).to.equal(200);
+      cy.url().should('be.equal', `${Cypress.config('baseUrl')}home`);
+      cy.get('#logoutButton').click();
+      cy.waitUntil(() =>
+        cy
+          .get('#loginPageTitle')
+          .should('have.text', 'CashManager')
+          .url()
+          .should('be.equal', `${Cypress.config('baseUrl')}login`)
+      ).then(() => expect(localStorage.getItem('user')).to.be.null);
+    });
   });
 });
