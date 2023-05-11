c66897416c825f7e53cdaa53674070a07f2debd1
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2023-05-11 21:29:30 +0200

    lint

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,21 +1,16 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterTestingModule } from '@angular/router/testing';
-import {
-  createComponentFactory,
-  createHttpFactory,
-  Spectator,
-  SpectatorHttp
-} from '@ngneat/spectator/jest';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+
 import { AuthService } from '../services/auth.service/auth.service';
 import { ConfigService } from '../services/config.service/config.service';
 import { ErrorHandlerService } from '../services/error.handler.service';
-
-import { HeaderComponent } from './header.component';
 import { ThemeService } from '../services/theme.service/theme.service';
-import { MatMenuModule } from '@angular/material/menu';
+import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let spectator: Spectator<HeaderComponent>;
