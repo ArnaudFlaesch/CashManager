17687180311948aef1423eff40c07e253697202e
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2023-12-26 16:02:13 +0100

    Code fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
+import { inject, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ErrorComponent } from './pages/error/error.component';
-import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { AuthService } from './services/auth.service/auth.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [() => inject(AuthService).userHasValidToken()]
+  },
   { path: 'error', component: ErrorComponent },
   { path: '**', redirectTo: 'home' }
 ];
